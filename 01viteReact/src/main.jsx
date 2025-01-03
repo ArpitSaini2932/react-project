ad778d07c8a4d2aa3eabfe638274a58005931ff9
Arpit Saini <vishusaini2932@gmail.com>
2025-01-03 23:10:17 +0530

    Create a custom element in React

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode> 
+// Making a custom element in React
+
+const NewCustomElement = React.createElement(
+  "p",
+  null,
+    "click here visit google"
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    NewCustomElement
+ 
 )
