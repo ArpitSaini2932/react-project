cb9e33d74089201667008375a70d5171380a0695
Arpit Saini <vishusaini2932@gmail.com>
2025-08-14 21:12:21 +0530

    Half done

---
+++ 12_blogApp/src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config"
+
+import { Account, ID, Client } from "appwrite"
+
+
+export class AuthService {
+    client = new Client()
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteURL)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.login({ email, password })
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async GetCurrentUser() {
+  try {
+    return await this.account.get();
+  } catch (error) {
+    if (error.code === 401) {
+      return null; 
+    }
+    throw error;
+  }
+}
+
+
+    async logout(){
+        try {
+            return await this.account.deleteSession()
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+
+
+const authservice = new AuthService()
+
+export default authservice
